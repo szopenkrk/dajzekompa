5c4277bbcd9db2cfb35f0d1d370d2b7efc7a387c
Karol Kochaski <karolkochanski@gmail.com>
2020-04-09 02:43:17 +0200

    add terms inluded like file

--- src/client/pages/TermsPage/TermsPage.tsx
+++ src/client/pages/TermsPage/TermsPage.tsx
@@ -1,12 +1,16 @@
 /* Libraries */
 import React from 'react';
+import Terms from '../../assets/regulamin.html'
 
 /* Application files */
 import SubPage from 'client/pages/SubPage';
 
 export function TermsPage () {
     return (
+        <>
         <SubPage title="Regulamin"></SubPage>
+        <div dangerouslySetInnerHTML={Terms} ></div>
+        </>
     );
 }
 
