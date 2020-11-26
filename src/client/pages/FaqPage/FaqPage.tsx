fa59ca58476c955617ecce06cf17384cea7d0ee5
root <root@dajzekompa.pl>
2020-11-26 23:33:14 +0100

    Faq changes

--- src/client/pages/FaqPage/FaqPage.tsx
+++ src/client/pages/FaqPage/FaqPage.tsx
@@ -1,7 +1,6 @@
 /* Libraries */
 import React from 'react';
-import { makeStyles, Typography, Button, useMediaQuery, useTheme } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { makeStyles, Typography, useMediaQuery, useTheme } from '@material-ui/core';
 
 /* Application files */
 import Config from 'client/lib/config';
@@ -9,7 +8,6 @@ import Separator from 'client/components/Separator';
 import Faq from 'client/components/Faq';
 import SubPage from 'client/pages/SubPage';
 import FaqItem from 'client/components/FaqItem';
-import TechSpecTooltip from 'client/components/TechSpecTooltip';
 
 import imageFlowGiversDesktop from 'client/assets/images/faq-givers-desktop.png';
 import imageFlowGiversMobile from 'client/assets/images/faq-givers-mobile.png';
@@ -59,7 +57,6 @@ export function FaqPage () {
     const theme = useTheme();
     const mobile = useMediaQuery(theme.breakpoints.down('sm'));
     const classes = useStyles();
-    const host = window.location.host;
     const mail = Config.CONTACT_EMAIL;
 
     return (
@@ -67,40 +64,49 @@ export function FaqPage () {
             <Typography variant="h6">Weźże mi wytłumacz...</Typography>
             <Faq title="Dla Kompo-dawców" showToggleAll={!mobile} className={classes.faq}>
                 <img src={mobile ? imageFlowGiversMobile : imageFlowGiversDesktop} className={classes.image} />
-                <FaqItem title="Jaki sprzęt jest potrzebny?">
-                    Szukamy głównie komputerów stacjonarnych, laptopów, tabletów, monitorów, klawiatur, myszek, kamerek, zestawów słuchawkowych, drukarek i innych <b>działających</b> urządzeń, które umożliwią dzieciom i nauczycielom podłączenie się do sieci i realizację nauki on-line.
+                <FaqItem title="Jakiego rodzaju sprzęt jest potrzebny?">
+                    Szukamy głównie komputerów stacjonarnych i laptopów. Dodatkowo: tabletów, monitorów, klawiatur, myszek, kamerek, zestawów słuchawkowych, drukarek i innych działających urządzeń, które umożliwią dzieciom i nauczycielom podłączenie się do sieci i realizację nauki on-line.
                 </FaqItem>
-                <FaqItem title="Czy mój komputer się nadaje?">
-                    Nieważny model, Mac vs. PC, Dell czy Acer, iPad vs Tablet Android - znajdziemy dobry dom dla twojego sprzętu*, o ile jest w stanie obsłużyć nowoczesną przeglądarkę internetową, ma <TechSpecTooltip>procesor 2,5 GHz (odpowiednik Intel Core 2 Duo i wzwyż), 4 GB RAM oraz kartę sieciową wi-fi (dowolną)</TechSpecTooltip>. Zalecany system Windows 10, ale jeśli go nie masz, to damy sobie radę. Generalnie, jeśli komputer nie jest bardzo stary, da się go odpalić, można na nim pisać i wszystko widać to wystarczy. Zanim sprzęt dotrze do dzieci i nauczycieli, wyślemy go do fachowca dla sprawdzenia, wgrania aktualnego oprogramowania i testowego uruchomienia. Ciebie natomiast prosimy o wyczyszczenie sprzętu z wszelkich danych zanim nastąpi jego przekazanie.
+                <FaqItem title="Skąd mam wiedzieć czy mój komp jest potrzebny?">
+                Nieważny model, Mac vs. PC, Dell czy Acer, iPad vs Tablet Android, o ile jest w stanie obsłużyć
+nowoczesną przeglądarkę internetową, ma procesor 2,5 GHz (odpowiednik Intel Core 2 Duo i
+wzwyż), 4 GB RAM oraz kartę sieciową WI-FI (dowolną). Zalecany system to Windows 10, ale
+jeśli go nie masz, to damy sobie radę. Prosimy o niewysyłanie elektrośmieci. Potrzebujemy
+działającego sprzętu, więc jeśli Twój Commodore64 w piwnicy oddał swój ostatni oddech w lat
+90-tych, to niech tam zostanie. Zanim sprzęt dotrze do dzieci i nauczycieli, wyślemy go do
+fachowca dla sprawdzenia, wgrania aktualnego oprogramowania i testowego uruchomienia.
                 </FaqItem>
                 <FaqItem title="Jak zgłosić sprzęt do oddania?">
-                    Zrób zdjęcia wszystkim sprzętom, które chcesz przekazać. Wejdź na stronę www.dajzekompa.pl do zakładki „Podaruj Kompa”, wypełnij formularz i wgraj zdjęcia. Następnie czekaj na instrukcje, które zostaną przesłane na podany przez Ciebie adres e-mail. Poprosimy Cię o wydrukowanie etykiety, którą przykleisz na paczkę, a także o dopełnienie koniecznych formalności.
+                Skontaktuj się z nami przez wiadomość mailową na adres: dajzekompa@polandbusinessrun.pl
                 </FaqItem>
                 <FaqItem title="Jak mam przygotować sprzęt?">
-                    Zgraj wszystkie dane osobiste, po czym koniecznie zrób systemowy reset i usuń wszelkie dane, które się na nim znajdują. Następnie zapakuj sprzęt tak, jakbyś chciał go sprzedać na aukcji: jak najwięcej folii bąbelkowej dookoła i tak, by był spakowany do jednego pudełka.
+                Zgraj wszystkie dane osobiste, możesz także zrobić systemowy reset. Jeśli nie będziesz
+umiał/mógł tego zrobić dane wyczyści nasz serwis komputerowy. Zapakuj sprzęt tak, jakbyś
+chciał go sprzedać na aukcji: jak najwięcej folii bąbelkowej dookoła, papierowy karton. Tak, aby
+trafił bezpiecznie do firmy serwisującej.
                 </FaqItem>
                 <FaqItem title="Jak wysłać paczkę?">
-                    Dostawcą paczek będzie firma InPost. Podczas rejestracji zostaniesz poproszony o podanie swojego adresu e-mail, na który otrzymasz instrukcje oraz etykietę paczki InPost. Poprosimy Cię o jej wydrukowanie i przyklejenie na wierzchu paczki.
+                Razem omówimy formę odbioru sprzętu od Ciebie, natomiast obdarowany będzie mógł go odebrać poprzez Paczkomat.
                 </FaqItem>
                 <FaqItem title="Jak moje dane osobowe będą chronione?">
-                    Chronimy Twoje dane zgodnie z przepisami RODO. Twoje dane będą wykorzystywane tylko i wyłącznie na potrzeby akcji przez podmioty do tego upoważnione i nie będą przekazywane osobom trzecim. <Link to="/polityka-prywatnosci" className={classes.link}>Tutaj</Link> możesz zobaczyć regulamin opisujący szczegółowo politykę prywatności.
+                Chronimy Twoje dane zgodnie z przepisami RODO. Twoje dane będą wykorzystywane tylko i wyłącznie na potrzeby akcji przez podmioty do tego upoważnione i nie będą przekazywane osobom trzecim. Tutaj możesz zobaczyć regulamin opisujący szczegółowo politykę prywatności.
                 </FaqItem>
                 <FaqItem title="Skąd mam pewność, że mój komputer trafi do osoby potrzebującej?">
-                    Listę uczniów i nauczycieli, którzy samodzielnie zgłoszą potrzebę poprzez stronę www.dajzekompa.pl zweryfikuje Fundacja Poland Business Run w kontakcie z dyrektorami szkół. Komputery trafią do dzieci zagrożonych wykluczeniem cyfrowym oraz nauczycieli, którzy bez odpowiedniego sprzętu nie mogą realizować programu w sposób zdalny.
+                Listę uczniów i nauczycieli, którzy samodzielnie zgłosili potrzebę poprzez stronę www.dajżekompa.pl zweryfikowali poprzez kontakt z Fundacją Poland Business Run dyrektorzy szkół. Komputery trafią tylko do dzieci zagrożonych wykluczeniem cyfrowym oraz nauczycieli, którzy bez odpowiedniego sprzętu nie mogą realizować programu w sposób zdalny.
                 </FaqItem>
                 <FaqItem title="Co w sytuacji, kiedy po wysyłce sprzętu okaże się, że z przyczyn technicznych nie jest możliwe jego serwisowanie?">
-                    Po podpisaniu umowy lub wystawieniu FV właścicielem sprzętu staje się Fundacja Poland Business Run. Jeśli w trakcie serwisowania okaże się, że nie spełnia on wymagań technicznych, fundacja wraz z pracownikami serwisu podejmie decyzję na temat dalszego losu sprzętu (może zostać rozebrany na części lub zutylizowany). Prosimy raz jeszcze, byś zwrócił uwagę na minimalne dane techniczne sprzętu.
+                Po podpisaniu umowy lub wystawieniu FV właścicielem sprzętu staje się Fundacja Poland Business Run. Jeśli w trakcie serwisowania okaże się, że nie spełnia on wymagań technicznych, fundacja wraz z pracownikami serwisu podejmie decyzję na temat dalszego losu sprzętu (może zostać rozebrany na części lub zutylizowany). Prosimy raz jeszcze byś zwrócił uwagę na minimalne dane techniczne sprzętu.
                 </FaqItem>
                 <FaqItem title="Czy ponoszę jakieś koszty związane z udziałem w akcji?">
-                    Darczyńca nie ponosi żadnych kosztów. Odbiór komputera i jego przesyłkę zapewnia InPost, serwisowaniem zajmuje się firma Fixit. Po Twojej stronie zostaje tylko zgłoszenie sprzętu w formularzu, podpisanie umowy darowizny lub wystawienie faktury oraz przygotowanie paczki do wysyłki i sama wysyłka przez paczkomat.
+                Darczyńca nie ponosi żadnych kosztów. Odbiór komputera zapewnia akcja (m.in. InPost), serwisowaniem zajmuje się firma FIXit. Po Twojej stronie zostaje tylko zgłoszenie sprzętu w formularzu, podpisanie umowy darowizny lub wystawienie faktury oraz przygotowanie komputera do wysyłki.
                 </FaqItem>
                 <FaqItem title="A co z podatkiem?">
-                    Zarówno darczyńca, jak i odbiorca nie muszą się martwić o kwestie podatkowe. Konieczne jest jednak wykonanie kilku czynności formalnych opisanych w <Link to="/regulamin" className={classes.link}>Regulaminie</Link>. W e-mailu, który otrzymasz po wypełnieniu formularza na stronie www.dajzekompa.pl, dostaniesz odpowiednie instrukcje. Ich wykonanie jest konieczne ze względów prawnych i podatkowych.
+                Zarówno darczyńca, jak i odbiorca nie muszą się martwić o kwestie podatkowe. Konieczne jest jednak wykonanie kilku prostych czynności. Zostaniesz poproszony o wypełnienie umowy darowizny (dotyczy osób prywatnych lub firm) lub faktury (dotyczy firm) na kwotę jednej złotówki i przesłanie jej do Fundacji Poland Business Run. Jest to konieczne ze względów podatkowych.
                 </FaqItem>
-                <FaqItem title="Czy #DajżeKompa działa w moim mieście?">
-                    Zaczęliśmy od Krakowa, mamy jednak nadzieję, że inicjatywa szybko ogarnie inne miasta. Zobacz na mapie czy tak się stało. Jeśli nie, może znasz ludzi, którzy chcieliby przejąć akcję w Twoim mieście. <Link to="/zglos-miasto" className={classes.link}>Tutaj</Link> możesz zobaczyć jak to zrobić.
+                <FaqItem title="Gdzie działacie?">
+                Działamy na terenie Krakowa i pomagamy krakowskim rodzinom. Współpracujemy głównie z lokalnymi firmami, ale nie tylko. Jeśli jesteś z innej części Polski czy nawet świata i masz nieużywany sprzęt komputerowy, prosimy, skontaktuj się z nami.
                 </FaqItem>
-                <FaqItem title="A co z podatkami, kiedy chcę przekazać laptopa lub tablet?">
+                {/* <FaqItem title="A co z podatkami, kiedy chcę przekazać laptopa lub tablet?">
                     Przekazanie laptopów oraz tabletów nastąpi w formie darowizny. W wiadomości e-mail prześlemy Ci odpowiednią umowę do wypełnienia. Na podstawie aktualnego Rozporządzenia darowizna laptopów i tabletów, które nam przekazujesz, jest opodatkowana stawką 0%. Oznacza to, że nie będziesz musiał zapłacić VATu od darowizny, ani dokonywać ewentualnej korekty VATu naliczonego. Wciąż jednak występuje konieczność wykazania tej transakcji w deklaracji VAT jako sprzedaż opodatkowana stawką 0% - pole 13 deklaracji VAT-7(20), wskazując jako podstawę opodatkowania wartość rynkową laptopa lub tabletu. Powiadom o tym swojego księgowego.
                 </FaqItem>
                 <FaqItem title="A co z podatkami, kiedy chcę przekazać sprzęt innego rodzaju?">
@@ -115,11 +121,8 @@ export function FaqPage () {
                 </FaqItem>
                 <FaqItem title="Czy można zawieźć sprzęt osobiście? Gdzie?">
                 Niestety nie ma takiej opcji. Chcemy, aby przekazywanie sprzętu odbywało się bezkontaktowo, dlatego też zachęcamy do korzystania z paczkomatów.
-                </FaqItem>
+                </FaqItem> */}
             </Faq>
-            <div className={classes.actions}>
-                <Link to="/podaruj-kompa"><Button variant="contained" color="primary">Podaruj kompa</Button></Link>
-            </div>
             <Typography variant="caption">
                 * Prosimy o niewysyłanie elektrośmieci. Potrzebujemy działającego sprzętu. Więc jeśli Twój Commodore64 w piwnicy oddał swój ostatni oddech w lat 90-tych, to niech tam zostanie.
             </Typography>
@@ -127,34 +130,25 @@ export function FaqPage () {
             <Faq title="Dla Kompo-biorców" showToggleAll={!mobile} className={classes.faq}>
                 <img src={mobile ? imageFlowReceiversMobile : imageFlowReceiversDesktop} className={classes.image} />
                 <FaqItem title="Do kogo skierowana jest akcja?">
-                    Akcja ma na celu wsparcie krakowskich uczniów oraz nauczycieli, którzy na skutek braku lub niewystarczającej jakości posiadanego sprzętu nie są w stanie realizować w pełni nauki w formie on-line.
-                </FaqItem>
-                <FaqItem title="Co zrobić, jeśli potrzebuję sprzętu? Gdzie mam się zgłosić?">
-                    Wypełnij formularz zgłoszeniowy dostępny na naszej stronie w zakładce Zgłoś Potrzebę, zaznaczając w nim numer szkoły, do której uczęszcza Twoje dziecko/w której uczysz. O weryfikację list organizatorzy poproszą dyrektorów szkół, którzy pomogą w wyborze osób najbardziej potrzebujących. Osoby, które zostaną wytypowane do otrzymania sprzętu dostaną od nas powiadomienie e-mailowe.
+                Akcja ma na celu wsparcie krakowskich uczniów oraz nauczycieli, którzy na skutek braku lub niewystarczającej jakości posiadanego sprzętu nie są w stanie realizować w pełni nauki w formie on-line.
                 </FaqItem>
                 <FaqItem title="Czy akcja dotyczy tylko szkół państwowych czy również prywatnych?">
-                    Akcja jest realizowana w ścisłej współpracy z Urzędem Miasta Krakowa i dotyczy szkół publicznych na terenie miasta.
-                </FaqItem>
-                <FaqItem title="Jestem dyrektorem/pracownikiem społecznym i znam osobę, która potrzebuje sprzętu. Jak mam zgłosić potrzebę?">
-                    Poproś rodziców/opiekunów prawnych dziecka lub nauczyciela, który potrzebuje sprzętu o zgłoszenie się przez formularz na stronie {host}. Już dzisiaj z góry dziękujemy za pomoc w dotarciu do osób najbardziej potrzebujących.
+                Akcja jest realizowana w ścisłej współpracy z Urzędem Miasta Krakowa i dotyczy szkół publicznych na terenie miasta.
                 </FaqItem>
                 <FaqItem title="Jak sprzęt dotrze do potrzebującego?">
-                    Po serwisie i dezynfekcji sprzętu, którym zajmuje się firma Fixit, sprzęt zostanie przekazany firmie InPost, która zadba o jej doręczenie do właściwego paczkomatu, zgodnego z adresem zamieszkania osoby potrzebującej. Odbiorca dostanie powiadomienie na swój telefon, gdy paczka będzie gotowa do odbioru i będzie miał 48h na wyjęcie jej z paczkomatu zlokalizowanego możliwie jak najbliżej jego miejsca zamieszkania.
+                Po serwisie i dezynfekcji sprzętu, którym zajmuje się firma Fixit, sprzęt zostanie przekazany firmie InPost, która zadba o jej doręczenie do właściwego paczkomatu, zgodnego z adresem zamieszkania osoby potrzebującej. Odbiorca dostanie powiadomienie na swój telefon, gdy paczka będzie gotowa do odbioru i będzie miał 48h na wyjęcie jej z paczkomatu zlokalizowanego możliwie jak najbliżej jego miejsca zamieszkania.
                 </FaqItem>
                 <FaqItem title="Co w przypadku, gdy paczka nie zostanie odebrana we właściwym czasie?">
-                    Na odbiór paczki masz 48 godzin. Po tym czasie sprzęt wróci do firmy Fixit i będziemy kontaktować się z odbiorcą w celu sprawdzenia sytuacji. Komputer może zostać przyznany innej potrzebującej osobie.
+                Na odbiór paczki masz 48 godzin. Po tym czasie sprzęt wróci do firmy Fixit i będziemy kontaktować się z odbiorcą w celu sprawdzenia sytuacji.
                 </FaqItem>
                 <FaqItem title="Czy sprzęt będzie bezpieczny (poddany dezynfekcji)?">
                     Tak, każdy przekazany nam sprzęt poddawany jest 24-godzinnej kwarantannie w firmie serwisującej. Następnie jest wyczyszczony, a na koniec, po wykonaniu wszelkich czynności serwisujących zdezynfekowany. Po odebraniu paczki najlepiej odczekać 24 godziny przed jej otwarciem.
                 </FaqItem>
-                <FaqItem title="Jestem opiekunem obdarowanego dziecka, czy zapłacę podatek?">
-                    Nie będziesz musiał płacić podatku. Będziesz musiał jednak zaznaczyć odpowiednie zgody na stronie {host} podczas zgłaszania potrzeby na sprzęt.
-                </FaqItem>
-                <FaqItem title="Czy jako obdarowany ponoszę jakiekolwiek koszty związane z udziałem w akcji?">
-                    Nie, cała akcja dzięki wsparciu krakowskich firm oraz Urzędu Miasta Krakowa jest tak przygotowana, by osoba potrzebująca nie musiała ponosić żadnych kosztów związanych ze sprzętem lub jego dostarczeniem.
+                <FaqItem title="Czy jako obdarowany poniosę jakiekolwiek koszty związane z udziałem w akcji?">
+                Nie, cała akcja dzięki wsparciu krakowskich firm oraz Urzędu Miasta Krakowa jest tak przygotowana, by osoba potrzebująca nie musiała ponosić żadnych kosztów związanych ze sprzętem lub jego dostarczeniem.
                 </FaqItem>
                 <FaqItem title="Na jak długo otrzymam sprzęt?">
-                    Sprzęt trafia do obdarowanego na stałe. Liczymy na to, że pomoże Twojemu dziecku/Tobie w nauce on-line i rozwoju, nie tylko w czasie kwarantanny, ale także później.
+                Sprzęt trafia do obdarowanego na zawsze. Liczymy na to, że pomoże mu w nauce i rozwoju nie tylko w czasie kwarantanny, ale także później.
                 </FaqItem>
                 <FaqItem title="Nie mam Internetu, co mam zrobić?">
                     Celem naszej akcji jest zaopatrzenie Twojego dziecka/Ciebie w sprawny sprzęt pozwalający na jak najlepszą realizację nauki on-line. Nie zapewniamy dostępu do Internetu. Wiemy jednak, że jest wiele firm i fundacji, które w dobie pandemii zdecydowały się zająć tą potrzebą. Kontakt z nimi jest po Twojej stronie. <b>Będziemy</b> starać się <b>informować</b> o takich inicjatywach za pośrednictwem naszych kanałów komunikacji.
@@ -219,3 +213,4 @@ export function FaqPage () {
 }
 
 export default FaqPage;
+
